29380387d134303eeb6cc59e101eea11ba63ca2b
giahck <gianluca.giacchetta@gmail.com>
2024-03-05 15:53:28 +0100

    m2-s3-g2

--- Mese2/settimana3/giorno2/assets/script/script.js
+++ Mese2/settimana3/giorno2/assets/script/script.js
@@ -4,10 +4,10 @@ const formName=document.getElementById('nome');
 const formCognome=document.getElementById('cognome');
 let tempo,i=0;
 btnAgiunggi.addEventListener('click', e=>{
-  i=0;
+  i=0;/* salva il nome e il cognme sul local */
   localStorage.setItem('nome',formName.value,'cognome',formCognome.value);
-  localStorage.setItem('cognome',formCognome.value);
-  if (formName!='' && formCognome!='') {
+  localStorage.setItem('cognome',formCognome.value);/* se metto il time qui posso dire che mi prende il timer totale dal primo accesso creato dell'utente */
+  if (formName!='' && formCognome!='') {/* dopo il click attivo la session  con nome e cognome */
     sessionStorage.setItem('name',localStorage.getItem('nome'))
     sessionStorage.setItem('cognome',localStorage.getItem('cognome'))
     sessionStorage.setItem('time',0)
@@ -27,7 +27,7 @@ function time(){
     i++;
   },1000)
 }
-if(sessionStorage.getItem('time')!=null){
+if(sessionStorage.getItem('time')!=null){/* unavolta avviato se riavvio il time continua */
   i=sessionStorage.getItem('time')
   time();
 }
