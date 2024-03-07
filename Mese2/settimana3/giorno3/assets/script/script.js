139d251aadd4b9a70429cc5b50ddad9eb69889dd
giahck <gianluca.giacchetta@gmail.com>
2024-03-07 09:27:37 +0100

    mese2-s3-g3

--- Mese2/settimana3/giorno3/assets/script/script.js
+++ Mese2/settimana3/giorno3/assets/script/script.js
@@ -1,36 +1,36 @@
 let elenco = [];
 
-window.addEventListener('load',init);
-function init(){
-    patch();
+window.addEventListener("load", init);
+function init() {
+  patch(); //lo faccio sotto e non a cascata cosi provo la Promise
 }
 
-async function patch(){
-    await fetch('https://striveschool-api.herokuapp.com/books')
-    .then(element=>{
-        return element.json();
+async function patch() {
+  await fetch("https://striveschool-api.herokuapp.com/books")
+    .then((element) => {
+      return element.json();
     })
-    .then(element=>{
-        elenco=element;//attendo 10m per carico elenco anche se non seriverebbe visto che la paggiana la iniziio diretamente dal load
-        console.log(elenco);
-        setTimeout(()=>{
-            caricoCard();
-        },100)
+    .then((element) => {
+      elenco = element; //attendo 10m per carico elenco anche se non seriverebbe visto che la paggiana la iniziio diretamente dal load
+      console.log(elenco);
+      setTimeout(() => {
+        caricoCard();
+      }, 100);
     })
-    .catch(err => {
-        console.log('Errore nel recupero dei dati: ', err);
+    .catch((err) => {
+      console.log("Errore nel recupero dei dati: ", err);
     });
 }
 
-function caricoCard(){
-    const container=document.getElementById('container');
-    const row=document.createElement('div');
-    row.setAttribute('class','row');
-    container.appendChild(row);
-    for(let elem of elenco){       
-    const col=document.createElement('div');   
-    col.setAttribute('class','col-3 col-xl-2')
-    col.innerHTML=`<div class="card pb-2 mb-3">
+function caricoCard() {
+  const container = document.getElementById("container");
+  const row = document.createElement("div");
+  row.setAttribute("class", "row");
+  container.appendChild(row);
+  for (let elem of elenco) {
+    const col = document.createElement("div");
+    col.setAttribute("class", "col-3 col-xl-2");
+    col.innerHTML = `<div class="card pb-2 mb-3">
     <img src="${elem.img}" class="card-img-top w-100"  alt="${elem.title}">
     <div class="card-body">
       <h2 class="card-title ">${elem.title}</h2>
@@ -42,19 +42,24 @@ function caricoCard(){
       </span>
       </div>
       <div class="d-flex justify-content-center ">
-      <button type="button" class="btn btn-light mx-2" id='add' vlaue='${elem.asin}'>Aggiungi</button>
+      <button type="button" class=" add btn btn-light mx-2" id='add' value='${elem.asin}'>Aggiungi</button>
       <button type="button" class="btn btn-danger mx-2">Elimina</button>
       </div>
-  </div>`    
+  </div>`;
     row.appendChild(col);
-    }
+  }
+  aggiungi();
 }
 
+aggiungi = () => {
+  const btnAdd = document.getElementsByClassName('add'); 
 
+   for(const element of btnAdd){
+    element.addEventListener('click',function () {
+        const libro=elenco.find((elemento)=>elemento.asin===this.value);
+        localStorage.setItem(`carrello${this.value}`,JSON.stringify(libro));
+    })
+  };
+ 
 
-const btnAdd=document.getElementById('add')
-btnAdd.addEventListener('click', function () {
-    // return new Promise((resolve,reject)=>{
-    //     set
-    // })
-})
+};
