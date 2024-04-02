eee0f4e0839c171bc725d4178a430f1c45eb687c
gianluca <gianluca.giacchetta@gmail.com>
2024-04-02 17:03:51 +0200

    loginM3-s3-g1

---
+++ Mese3/settimana3/giorno1/esercizio/src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { User } from './../../../../../settimana2/giorno2/postApp/src/app/models/user.interface';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'esercizio';
+  formSub!: FormGroup;
+  user!: User;
+  genere = ['Uomo', 'Donna','LGBT+'];
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formSub = this.fb.group({
+      name: (''),
+      cognome: (''),
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      passwordConfirm: ['', [Validators.required]],
+      gender:this.fb.control('')
+    }, { validator: this.passwordMatchValidator });
+
+    this.formSub.valueChanges.subscribe((status) => {
+      console.log('Stato del form: ', status);
+    });
+    
+  }
+
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const passwordConfirm = control.get('passwordConfirm')?.value;
+  
+    return password === passwordConfirm ? null : { 'passwordMismatch': true };
+  };
+
+  getPasswordConfirm(): { passwordMatch: boolean } {
+    const password = this.formSub.get('password')?.value;
+    const passwordConfirm = this.formSub.get('passwordConfirm')?.value;
+    return { passwordMatch: password === passwordConfirm };
+  }
+
+  getUser(name: string): any {
+    return this.formSub.get(name);
+  }
+
+  submit(): void {
+    this.user=this.formSub.value
+    this.user.imgLink='https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png'
+    console.log(this.user);
+    this.formSub.reset();
+  }
+}
---
+++ Mese3/settimana3/giorno1/esercizio/src/app/interface/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    username:String,
+    nome:String,
+    cognome:string,
+    password:String,
+    genere:string,
+    imgLink:string,
+    biografia:string
+}
