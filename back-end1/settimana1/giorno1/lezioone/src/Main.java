7b1db50c0d747cd8193fa963b408eb407ec934f6
gianluca <gianluca.giacchetta@gmail.com>
2024-04-15 12:58:54 +0200

    esercizio3

--- back-end1/settimana1/giorno1/lezioone/src/Main.java
+++ back-end1/settimana1/giorno1/lezioone/src/Main.java
@@ -1,32 +1,52 @@
 import java.util.Scanner;
 
 public class Main {
-      static String[] fiveString = new String[5];
-       static Scanner scanner = new Scanner(System.in);
+    static String[] fiveString = new String[5];
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-      //  stampa();
+          stampa();
         popolaArrey();
-        inserisciElemInAerry("riga inserita nella posizione 4",3);
+        String[] sixString = inserisciElemInAerry("riga inserita nella posizione 4", 3);
+
+        conactString(sixString);
     }
-    public static void inserisciElemInAerry(String newLine,int pos){
-       String[] sixString=new String[fiveString.length+1];
-       System.arraycopy(fiveString,0,sixString,0,pos);
-       sixString[pos]=newLine;
-         System.arraycopy(fiveString,pos,sixString,pos+1,fiveString.length-pos);
-        for(String elem:sixString){
+
+    public static void conactString(String[] sixString) {
+        String Concat = "", ConcatReverse = "";
+        for (String elem : sixString) {
+            Concat += elem + " ";
+        }
+        for(int i=sixString.length-1;i>=0;i--){
+            ConcatReverse+=sixString[i]+" ";
+        }
+        System.out.println("concatena in ordine di inserimento "+Concat);
+        System.out.println("concatena in ordine reversso "+ConcatReverse);
+        scanner.close();
+    }
+
+    public static String[] inserisciElemInAerry(String newLine, int pos) {
+        String[] sixString = new String[fiveString.length + 1];
+        System.arraycopy(fiveString, 0, sixString, 0, pos);
+        sixString[pos] = newLine;
+        System.arraycopy(fiveString, pos, sixString, pos + 1, fiveString.length - pos);
+        /*for(String elem:sixString){
             System.out.println(elem);
-        }
+        }*/
+        return sixString;
+
     }
-    public static void popolaArrey(){
+
+    public static void popolaArrey() {
+            scanner.nextLine();
         for (int i = 0; i < 5; i++) {
-            System.out.println("inserisci "+(i+1)+" stringa");
-            fiveString[i]=scanner.next();
-
+            System.out.println("inserisci " + (i + 1) + " stringa");
+            fiveString[i] = scanner.nextLine();
         }
 
     }
-    public static void stampa(){
+
+    public static void stampa() {
         System.out.println("inserisci primo numero");
         int a = scanner.nextInt();
         System.out.println("inserisci secondo numero");
@@ -35,17 +55,18 @@ public class Main {
         System.out.println(moltipliazione(a, b));
         //stampa la concatenazione della moltiplicazione e della stringa
         System.out.println("inserisci testo");
-        String text=scanner.next();
+        String text = scanner.next();
         System.out.println(concatena(moltipliazione(a, b), text));
+
     }
 
-    public static int moltipliazione(int a, int b)
-        {
-            return a*b;
-        }
-        public static String concatena(int multi,String text){
+    public static int moltipliazione(int a, int b) {
+        return a * b;
+    }
 
-            return multi + text;
-        }
+    public static String concatena(int multi, String text) {
+
+        return multi + text;
+    }
 
 }
