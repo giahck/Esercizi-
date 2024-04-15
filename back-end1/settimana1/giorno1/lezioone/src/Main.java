144f2a2484a1e5a22feee69057fa8acab5093520
gianluca <gianluca.giacchetta@gmail.com>
2024-04-15 15:57:17 +0200

    esercizi

--- back-end1/settimana1/giorno1/lezioone/src/Main.java
+++ back-end1/settimana1/giorno1/lezioone/src/Main.java
@@ -1,15 +1,33 @@
 import java.util.Scanner;
 
+class Triangolo {
+    public float area(float l1, float l2, float l3) { //non restituisco qui
+        //A=rad(p*(p-a)*(p-b)*(p-c))
+        float a = (float) Math.sqrt((l1 + l2 + l3) / 2 * ((l1 + l2 + l3) / 2 - l1) * ((l1 + l2 + l3) / 2 - l2) * ((l1 + l2 + l3) / 2 - l3));
+        return a;
+    }
+
+
+        public void stampa ( float resulArea){
+            System.out.println("l'area del triangolo é " + resulArea);
+        }
+
+
+}
+
 public class Main {//esercizio 1 e 2 insieme il 3 é separato
     static String[] fiveString = new String[5];
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-          stampa();
+        stampa();
         popolaArrey();
         String[] sixString = inserisciElemInAerry("riga inserita nella posizione 4", 3);
 
         conactString(sixString);
+        Triangolo triangolo = new Triangolo();
+        float area = triangolo.area(10, 10, 10);
+        triangolo.stampa(area);
     }
 
     public static void conactString(String[] sixString) {
@@ -17,11 +35,11 @@ public class Main {//esercizio 1 e 2 insieme il 3 é separato
         for (String elem : sixString) {
             Concat += elem + " ";
         }
-        for(int i=sixString.length-1;i>=0;i--){
-            ConcatReverse+=sixString[i]+" ";
+        for (int i = sixString.length - 1; i >= 0; i--) {
+            ConcatReverse += sixString[i] + " ";
         }
-        System.out.println("concatena in ordine di inserimento "+Concat);
-        System.out.println("concatena in ordine reversso "+ConcatReverse);
+        System.out.println("concatena in ordine di inserimento " + Concat);
+        System.out.println("concatena in ordine reversso " + ConcatReverse);
         scanner.close();
     }
 
@@ -38,7 +56,7 @@ public class Main {//esercizio 1 e 2 insieme il 3 é separato
     }
 
     public static void popolaArrey() {
-            scanner.nextLine();
+        scanner.nextLine();
         for (int i = 0; i < 5; i++) {
             System.out.println("inserisci " + (i + 1) + " stringa");
             fiveString[i] = scanner.nextLine();
