f16a1c065c4fa5e655e314ce875b797532d5a6c2
gianluca <gianluca.giacchetta@gmail.com>
2024-04-30 15:59:57 +0200

    M4-S3-G2

--- back-end1/settimana3/giorno2/esercizio/src/main/java/esercizio/eventi/Main.java
+++ back-end1/settimana3/giorno2/esercizio/src/main/java/esercizio/eventi/Main.java
@@ -8,23 +8,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf= Persistence.createEntityManagerFactory("ese_gdue_mquattro");
         EntityManager em=emf.createEntityManager();
 
-        EventiDao eventiDao=new EventiDao(em);
         Eventi e1=new Eventi();
         e1.setTitolo("Festa di compleanno");
         e1.setDataEvento(java.time.LocalDate.of(2021, 10, 10));
         e1.setDescrizione("Festa di compleanno di Mario");
         e1.setTipoEvento(tipoEvento.FESTA);
         e1.setNumeroPartecipanti(10);
-        eventiDao.save(e1);
+        EventiDao eventiDao=new EventiDao(em);
+        //eventiDao.save(e1);
 //        Eventi ev1=eventiDao.getById(1);
 //        System.out.println(ev1);
 //        eventiDao.delete(ev1);
+        List<Eventi> eventiList= eventiDao.query();
+        eventiList.stream().filter(e->e.getId()==3).findFirst().ifPresent(e->e.setDescrizione("Festa di compleanno di Mario aggiornata"));
+        eventiDao.update(eventiList.get(1));
+
 
 
     }
--- back-end1/settimana3/giorno2/esercizio/src/main/java/esercizio/eventi/dao/EventiDao.java
+++ back-end1/settimana3/giorno2/esercizio/src/main/java/esercizio/eventi/dao/EventiDao.java
@@ -4,6 +4,9 @@ import esercizio.eventi.entity.Eventi;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class EventiDao {
     private EntityManager em;
@@ -24,5 +27,20 @@ public class EventiDao {
         t.begin();
         em.remove(eventi);
         t.commit();
+    }
+    public void update(Eventi eventi){
+        EntityTransaction t = em.getTransaction();
+        t.begin();
+        em.merge(eventi);
+        t.commit();
+    }
+    public List<Eventi> query(){
+        // Utilizzo di una query SQL diretta con createNativeQuery()
+        Query query = em.createNativeQuery("SELECT * FROM Eventi", Eventi.class);
+        query.setFirstResult(0);
+        query.setMaxResults(10);
+        // Eseguire la query e ottenere la lista dei risultati
+        List<Eventi> result = query.getResultList();
+        return  result;
     }
 }
