de67a1609297b326fe142c6de8552b4c8900dc23
gianluca <gianluca.giacchetta@gmail.com>
2024-05-28 17:38:37 +0200

    UPD

--- back-end2/settimana3/giorno2/progetto dispositivi aziendaili/pgM5S2G5/src/main/java/it/dispositiviAziendali/pgM5S2G5/exeption/NotFoundExecution.java
+++
@@ -1,7 +0,0 @@
-package it.dispositiviAziendali.pgM5S2G5.exeption;
-
-public class NotFoundExecution extends RuntimeException {
-    public NotFoundExecution(String message) {
-        super(message);
-    }
-}
