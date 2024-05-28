24ef602b70b269cc7dceb7c9e99352c6a8acbaa6
gianluca <gianluca.giacchetta@gmail.com>
2024-05-28 17:38:37 +0200

    UPD

--- back-end2/settimana3/giorno2/progetto dispositivi aziendaili/pgM5S2G5/src/main/java/it/dispositiviAziendali/pgM5S2G5/enums/EnumTipoSensore.java
+++
@@ -1,5 +0,0 @@
-package it.dispositiviAziendali.pgM5S2G5.enums;
-
-public enum EnumTipoSensore {
-    MOVIMENTO,TEMPERATURA,UMIDITA
-}
--- back-end2/settimana3/giorno2/progetto dispositivi aziendaili/pgM5S2G5/src/main/java/it/dispositiviAziendali/pgM5S2G5/repository/DipendenteRepository.java
+++
@@ -1,9 +0,0 @@
-package it.dispositiviAziendali.pgM5S2G5.repository;
-
-import it.dispositiviAziendali.pgM5S2G5.model.Dipendente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DipendenteRepository extends JpaRepository<Dipendente, Integer>{
-}
