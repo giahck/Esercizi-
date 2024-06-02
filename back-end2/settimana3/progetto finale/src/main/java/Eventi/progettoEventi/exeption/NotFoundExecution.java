9ee6a323ea99f63c8c876a3b8b0a4c6eb51dad98
gianluca <gianluca.giacchetta@gmail.com>
2024-06-02 18:17:15 +0200

    Rimuove la cartella progetto finale e i suoi contenuti

--- back-end2/settimana3/progetto finale/src/main/java/Eventi/progettoEventi/exeption/NotFoundExecution.java
+++
@@ -1,7 +0,0 @@
-package Eventi.progettoEventi.exeption;
-
-public class NotFoundExecution extends RuntimeException {
-    public NotFoundExecution(String message) {
-        super(message);
-    }
-}
